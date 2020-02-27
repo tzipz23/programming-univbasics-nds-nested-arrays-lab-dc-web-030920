8380fe7a98f23062d5ea833addc97007573ff4b2
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-27 21:20:24 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -62,8 +62,7 @@ end
 
 def matrix_update(matrix, row, column, new_value)
   
-  matrix_update(assembled_matrix[0][2]) = "Lemons"
-  matrix_update
+  
  
  
   # Given any matrix (array of arrays), a row index and a column index, 
