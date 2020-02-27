e79e6383797623135aa6faa1a5bd3eefba4109e1
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-27 19:26:39 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,19 +19,19 @@ CONVENTIONAL_PRODUCE = [
 def assembled_matrix
   
   assembled_matrix = [
-    ORGANIC_PRODUCE = [
-  "Strawberries",
-  "Potatoes",
-  "Grapes",
-  "Avocadoes",
-  "Asparagus"
-],
-CONVENTIONAL_PRODUCE = [
+  CONVENTIONAL_PRODUCE = [
   "Grapefruit",
   "Pineapple",
   "Oranges",
   "Watermelon",
   "Eggplant"
+],
+ORGANIC_PRODUCE = [
+  "Strawberries",
+  "Potatoes",
+  "Grapes",
+  "Avocadoes",
+  "Asparagus"
 ]
     ]
   
