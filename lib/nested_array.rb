4649481ed9ee103af7b5a4cc7fe752a1239ab1d0
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-27 20:21:43 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,8 +37,8 @@ def sorted_matrix
   ["Strawberries","Potatoes","Grapes","Avocadoes","Asparagus"]
 ]
  
-  sorted_matrix.sort[0]
-  sorted_matrix.sort[1]
+  sorted_matrix[0].sort
+  sorted_matrix[1].sort
   sorted_matrix
   
   # Using Array literal syntax only, build another nested array that 
