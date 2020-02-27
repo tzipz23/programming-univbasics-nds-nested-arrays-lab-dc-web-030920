fcf8843476809d66f0beb3eb5e51406fb38cead1
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-27 20:20:00 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,8 +38,9 @@ def sorted_matrix
 ]
  
   assembled_matrix.sort
+  sorted_matrix.sort
   
-  sorted_matrix
+
   
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
