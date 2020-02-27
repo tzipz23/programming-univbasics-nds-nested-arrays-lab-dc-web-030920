0a06f73a6c3ff289166e3718f328df827229631b
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-27 20:36:28 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -47,8 +47,8 @@ end
 
 def matrix_lookup(matrix, row, column)
     
-  assembled_matrix[1][1] 
-  sorted_matrix[1][1]
+  matrix_lookup = [ assembled_matrix[1][1],  
+  sorted_matrix[1][1]]
     
     
    
